e01cdec9228245998945319b538913c6a543ec2d
Ankush Sarkar <ankush.sarkar2002@gmail.com>
2020-01-06 10:15:10 +0530

    comands.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ import click
 from flask.cli import with_appcontext
 import os
 
-# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-# os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
 app = Flask(__name__)
 
