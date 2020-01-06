7e9f66ab65ec788d64621a3c9a485f88ab895339
Ankush Sarkar <ankush.sarkar2002@gmail.com>
2020-01-06 09:30:42 +0530

    comands.py

--- app/comands.py
+++ app/comands.py
@@ -1,7 +1,7 @@
 import click
 from flask.cli import with_appcontext
 
-from .models import db User, Question
+from .models import db, User, Question
 
 @click.command(name='create_tables')
 @with_appcontext
